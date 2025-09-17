f0fbf906d5f342d0455ebd325f43942a83543f7f
Schrieffer <xjack875@gmail.com>
2025-09-17 16:46:39 +0800

    test

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,17 @@
-#include <iostream>
+#include <unistd.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[]) 
 {
-    std::cout << "hello, world" << std::endl;
+    const char msg[] = "Hello, World!\n";
+    asm volatile (
+        "mov $1, %%rax\n\t"        // sys_write
+        "mov $1, %%rdi\n\t"        // fd = 1 (stdout)
+        "lea %[str], %%rsi\n\t"    // buf = msg
+        "mov %[len], %%rdx\n\t"    // len
+        "syscall\n\t"
+        :
+        : [str]"m"(msg), [len]"r"(sizeof(msg)-1)
+        : "rax", "rdi", "rsi", "rdx"
+    );
     return 0;
 }
