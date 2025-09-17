7c931362351a75e09b9b926f75d1c84aa4393482
Schrieffer <xjack875@gmail.com>
2025-09-17 19:33:20 +0800

    updata

--- hello.cpp
+++ hello.cpp
@@ -1,17 +1,7 @@
-#include <unistd.h>
+#include <iostream>
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-    const char msg[] = "Hello, World!\n";
-    asm volatile (
-        "mov $1, %%rax\n\t"        // sys_write
-        "mov $1, %%rdi\n\t"        // fd = 1 (stdout)
-        "lea %[str], %%rsi\n\t"    // buf = msg
-        "mov %[len], %%rdx\n\t"    // len
-        "syscall\n\t"
-        :
-        : [str]"m"(msg), [len]"r"(sizeof(msg)-1)
-        : "rax", "rdi", "rsi", "rdx"
-    );
+    std::cout << "hello, world" << std::endl;
     return 0;
 }
